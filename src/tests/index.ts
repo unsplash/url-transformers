dd3590b505a7f6eb9a7a9488531af4128ee3f096
Oliver Joseph Ash <oliverjash@gmail.com>
2019-12-12 12:52:38 +0000

    WIP

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -5,10 +5,8 @@ import { URL, URLSearchParams } from 'url';
 import {
     addSearchParamsInUrl,
     appendPathnameToUrl,
-    createAuthForFormat,
     modifyUrl,
     modifyUrlClass,
-    pathStringToObject,
     replaceHashInUrl,
     replacePathInUrl,
     replacePathnameInUrl,
@@ -17,24 +15,6 @@ import {
     replaceSearchParamsInURLObject,
 } from '../index';
 import './helpers';
-
-assert.deepEqual(createAuthForFormat({ username: '', password: '' }), undefined);
-assert.deepEqual(createAuthForFormat({ username: 'a', password: '' }), 'a');
-assert.deepEqual(createAuthForFormat({ username: 'a', password: 'b' }), 'a:b');
-
-assert.deepEqual(pathStringToObject(''), { pathname: '', searchParams: new URLSearchParams() });
-assert.deepEqual(pathStringToObject('/foo'), {
-    pathname: '/foo',
-    searchParams: new URLSearchParams(),
-});
-assert.deepEqual(pathStringToObject('/foo?a=b'), {
-    pathname: '/foo',
-    searchParams: new URLSearchParams({ a: 'b' }),
-});
-assert.deepEqual(pathStringToObject('?a=b'), {
-    pathname: '',
-    searchParams: new URLSearchParams({ a: 'b' }),
-});
 
 assert.deepEqual(
     modifyUrlClass(urlObject => ({
